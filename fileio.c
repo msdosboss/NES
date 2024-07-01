2bda52c5caef81ab1f83288ce738b51417c6f2e6
orcalord <jacobt0722@gmail.com>
2024-06-30 17:15:09 -0700

    removed unused code from fileio.c

--- fileio.c
+++ fileio.c
@@ -13,16 +13,9 @@ char *loadRom(char *fileName){
 	fseek(file, 0, SEEK_END);
 	long int size = ftell(file);
 	fseek(file, 0, SEEK_SET);
-	int lineCount = 0;
 
 	char *rom = malloc(sizeof(char) * size);
 	char temp[1024];
-
-	while(fgets(rom, size, file) != NULL){
-		lineCount++;
-	}
-	
-	fseek(file, 0, SEEK_SET);
 
 	for(int i = 0; fgets(&rom[i], size, file) != NULL; i = strlen(rom)){
 		continue;
