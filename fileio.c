bce9b96a61a10dca30313e9659d76cb77cc9f6ee
orcalord <jacobt0722@gmail.com>
2024-07-08 09:09:21 -0700

    fileio is working

--- fileio.c
+++ fileio.c
@@ -2,7 +2,56 @@
 
 #define MAXLINELENGTH 1024
 
-char *loadRom(char *fileName){
+int instructionCount(char *str){
+	int len = 0;
+	for(int i = 0; str[i] != '\0'; i++){
+		if(str[i] == ','){
+			len++;
+		}
+	}
+	return len + 1;
+}
+
+unsigned char hexToChar(char *hexVal){
+	unsigned char val = 0;
+	for(int i = 0; i < 2; i++){
+		if(hexVal[i] >= '0' && hexVal[i] <= '9'){
+			val = val * 16 + hexVal[i] - '0';
+		}
+		else{
+			val = val * 16 + ((hexVal[i] - 'a') + 10);
+		}
+	}
+	return val;
+}
+
+
+struct Rom parse(char *str){	//This func assumes the input is in this format (0x66, 0x22, ...)
+	int i = 0;
+	int j = 0;
+	struct Rom rom;
+	rom.len = instructionCount(str);
+	rom.hexVals = malloc(sizeof(unsigned char) * rom.len);
+	while(str[i] != '\0'){
+		while(str[i] != '\n' && str[i] != '\0'){
+			while(str[i] == ' '){
+				i++;
+			}
+			if(str[i] == ','){
+				rom.hexVals[j++] = hexToChar((&(str[i])) - sizeof(char) * 2);	//starts the string after 0x
+			}
+			i++;
+		}
+		i++;
+	}
+	rom.hexVals[j] = hexToChar((&(str[i - 1])) - sizeof(char) * 2);
+
+	return rom;
+	
+	
+}
+
+struct Rom loadRom(char *fileName){
 	FILE *file = fopen(fileName, "r");
 	if(file == NULL){
 		printf("%s is not a file\n", fileName);
@@ -12,13 +61,15 @@ char *loadRom(char *fileName){
 	long int size = ftell(file);
 	fseek(file, 0, SEEK_SET);
 
-	char *rom = malloc(sizeof(char) * size);
+	char *str = malloc(sizeof(char) * size);
 	char temp[1024];
 
-	for(int i = 0; fgets(&rom[i], size, file) != NULL; i = strlen(rom)){
+	for(int i = 0; fgets(&str[i], size, file) != NULL; i = strlen(str)){
 		continue;
 	}
-	
+
+	struct Rom rom = parse(str);
+
 	return rom;
 }
 
--- fileio.h
+++ fileio.h
@@ -5,6 +5,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *loadRom(char *fileName);
+struct Rom{
+	int len;
+	unsigned char *hexVals;
+};
+int instructionCount(char *str);
+unsigned char hexToChar(char *hexVal);
+struct Rom parse(char *str);
+struct Rom loadRom(char *fileName);
 
 #endif
