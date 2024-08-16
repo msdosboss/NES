f826d6679667631d6076e7d7c5420f7e8cb429e4
msdosboss <jacobt0722@gmail.com>
2024-08-16 08:39:39 -0700

    forgot to save all my additions to the ppu

--- ppu.c
+++ ppu.c
@@ -17,3 +17,32 @@ void initPPU(struct PPU *ppu, struct Rom *rom){
 	memset(ppu->oamData, 0, sizeof(ppu->oamData));
 	ppu->mirrorMode = rom->mirrorMode;
 }
+
+void initAddrRegister(struct AddrRegister *addr){
+	memset(addr->value, 0, sizeof(addr->value));
+	addr->hiByte = 1;
+}
+
+unsigned short getAddrRegister(struct AddrRegister *addr){ 
+	unsigned short fullAddr = ((addr->value[0] << 8) | addr->val[1]);
+	return fullAddr;
+}
+
+void setAddrRegister(struct AddrRegister *addr, unsigned short data){
+	addr->value[0] = (data >> 8);
+	addr->value[1] = data & 0xff;
+}
+
+void updateAddrRegister(struct AddrRegister *addr, unsigned char data){
+	if(addr->hiByte){
+		addr->value[0] = data;
+	}
+	else{
+		addr->value[1] = data;
+	}
+
+	if(getAddrRegister(addr) > 0x3fff){
+		setAddrRegister(addr, getAddrRegister(addr) & 0b11111111111111);	//mirror down addr above 0x3fff
+	}
+}
+
