cecc4cc4b8f43995ac2e370848af8d84bcf985d8
orcalord <jacobt0722@gmail.com>
2024-08-20 08:54:52 -0700

    added the control register

--- ppu.c
+++ ppu.c
@@ -10,6 +10,23 @@ struct PPU{
 };
 */
 
+   // 7  bit  0
+   // ---- ----
+   // VPHB SINN
+   // |||| ||||
+   // |||| ||++- Base nametable address
+   // |||| ||    (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
+   // |||| |+--- VRAM address increment per CPU read/write of PPUDATA
+   // |||| |     (0: add 1, going across; 1: add 32, going down)
+   // |||| +---- Sprite pattern table address for 8x8 sprites
+   // ||||       (0: $0000; 1: $1000; ignored in 8x16 mode)
+   // |||+------ Background pattern table address (0: $0000; 1: $1000)
+   // ||+------- Sprite size (0: 8x8 pixels; 1: 8x16 pixels)
+   // |+-------- PPU master/slave select
+   // |          (0: read backdrop from EXT pins; 1: output color on EXT pins)
+   // +--------- Generate an NMI at the start of the
+   //            vertical blanking interval (0: off; 1: on)
+
 void initPPU(struct PPU *ppu, struct Rom *rom){
 	ppu->chrRom = rom->chrRom;
 	memset(ppu->paletteTable, 0, sizeof(ppu->paletteTable));
@@ -31,6 +48,28 @@ unsigned short getAddrRegister(struct AddrRegister *addr){
 void setAddrRegister(struct AddrRegister *addr, unsigned short data){
 	addr->value[0] = (data >> 8);
 	addr->value[1] = data & 0xff;
+}
+
+void incrementAddrRegister(struct AddrRegister *addr){
+	int incAmount;
+	if((busRead(&(ppu->bus), 0x2000) & VRAM_ADD_INCREMENT)){	//0x2000 is the control register
+		incAmount = 32;	
+	}
+	else{
+		incAmount = 1;
+	}
+	unsigned char post = addr.value[1] + incAmount;
+	if(post < addr.value[1]){
+		addr.value[1] += incAmount;
+		addr.value[0] += 1;
+	}
+	else{
+		addr.value[1] += incAmount;
+	}
+	if(getAddrRegister(addr) > 0x3fff){
+		setAddrRegister(addr, getAddrRegister(addr) & 0b11111111111111);	//mirror down addr above 0x3fff
+	}
+
 }
 
 void updateAddrRegister(struct AddrRegister *addr, unsigned char data){
--- ppu.h
+++ ppu.h
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include "fileio.h"
 
+#define NAMETABLE1 0b00000001
+#define NAMETABLE2 0b00000010
+#define VRAM_ADD_INCREMENT 0b00000100
+#define SPRITE_PATTERN_ADDR 0b00001000
+#define BACKROUND_PATTERN_ADDR 0b00010000
+#define SPRITE_SIZE 0b00100000
+#define MASTER_SLAVE_SELECT 0b01000000
+#define GENERATE_NMI 0b10000000
+
 struct AddrRegister{
 	unsigned char value[2];
 	int hiByte;
